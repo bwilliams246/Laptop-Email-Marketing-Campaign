62d1fd39048b3b7383fe84fb00114eee45078a0e
Brian <bwilliams7622@gmail.com>
2022-11-15 00:44:39 -0600

    landing page form submits!

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const modal = document.getElementsByClassName('modal')[0];
 const openModal = document.getElementsByClassName('cta-add')[0];
+const mailBtn = document.getElementsByClassName('mail')[0];
 const closeBtn = document.getElementsByClassName('close-btn')[0];
 
 openModal.addEventListener('click' , () => {
+    modal.showModal();
+});
+
+mailBtn.addEventListener('click' , () => {
     modal.showModal();
 });
 
