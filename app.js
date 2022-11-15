15d07c0b936abe98a8cdb9f86f59f67abdf12b89
Brian <bwilliams7622@gmail.com>
2022-11-14 23:13:55 -0600

    modal opens and closes onclick

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const modal = document.getElementsByClassName('modal')[0];
 const openModal = document.getElementsByClassName('cta-add')[0];
+const closeBtn = document.getElementsByClassName('close-btn')[0];
 
 openModal.addEventListener('click' , () => {
     modal.showModal();
+});
+
+closeBtn.addEventListener('click' , () => {
+    modal.close();
 });
